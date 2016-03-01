26dbe968a7b9ddd5d1c9db251267a049f3bc58f3
ZeroLive <zerolive@gmail.com>
2016-03-01 19:50:14 +0100

    Created object createStars

--- js/create.js
+++ js/create.js
@@ -3,15 +3,13 @@ var gameAdd = function(){
 
   scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-  var platformsUnadded = [
+  var platformsToadded = [
     [0, (game.world.height - 64), 'ground', 2],
     [400, 400, 'ground', 1],
     [-150, 250, 'ground', 1]
   ];
 
-  platforms = game.add.group();
-    platforms.enableBody = true;
-    addPlatforms(platformsUnadded);
+  createPlatforms(platformsToadded);
 
   player = game.add.sprite(32, game.world.height - 150, 'dude');
     game.physics.arcade.enable(player);
@@ -21,24 +19,35 @@ var gameAdd = function(){
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-  stars = game.add.group();
-    stars.enableBody = true;
     createStars(12);
 };
 
-var addPlatforms = function(platformsUnadded){
-  for(var i = 0; i < platformsUnadded.length; i++){
-    var platform = platformsUnadded[i];
-    var ground = platforms.create(platform[0], platform[1], platform[2]);
-        ground.body.immovable = true;
-        ground.scale.setTo(platform[3], platform[3]);
+var createPlatforms = function(platformsToadded){
+  platforms = game.add.group();
+    platforms.enableBody = true;
+    addPlatforms(platformsToadded);
+
+  function addPlatforms(platformsToadded){
+    for(var i = 0; i < platformsToadded.length; i++){
+      var platform = platformsToadded[i];
+
+      var ground = platforms.create(platform[0], platform[1], platform[2]);
+            ground.body.immovable = true;
+            ground.scale.setTo(platform[3], platform[3]);
+    };
   };
 };
 
 var createStars = function(numberOfStars){
-  for(var i = 0; i < numberOfStars; i++){
-    var star = stars.create(i * 70, 0, 'star');
-      star.body.gravity.y = 300;
-      star.body.bounce.y = 0.7 + Math.random() * 0.2;
+  stars = game.add.group();
+    stars.enableBody = true;
+    addStars(12);
+
+  function addStars(numberOfStars){
+    for(var i = 0; i < numberOfStars; i++){
+      var star = stars.create(i * 70, 0, 'star');
+        star.body.gravity.y = 300;
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    };
   };
 };
